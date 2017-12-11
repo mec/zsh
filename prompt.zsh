15e564881c5683c6f77727678aadb7a73131e9e5
Mec Rawlings <mec.rawlings@gmail.com>
2017-12-11 22:57:07 +0000

    Arrows for git now.

--- prompt.zsh
+++ prompt.zsh
@@ -13,7 +13,7 @@ function git_prompt_info {
   elif [[ ${gitst} =~ "use \"git add" ]]; then
     gitstatus="%{$fg[grey]%}(${ref#refs/heads/} *)%{$reset_color%}"
   elif [[ -n `git checkout HEAD 2> /dev/null | grep ahead` ]]; then
-    gitstatus="%{$fg[yellow]%}(${ref#refs/heads/} up)%{$reset_color%}"
+    gitstatus="%{$fg[yellow]%}(${ref#refs/heads/} ⇡)%{$reset_color%}"
   else
     gitstatus="%{$fg[green]%}(${ref#refs/heads/})%{$reset_color%}"
   fi
